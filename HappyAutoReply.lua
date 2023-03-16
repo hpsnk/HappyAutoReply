2b6f3b8451e153e6053c57984cbfb10680883aa3
happysonic <archer03371@163.com>
2023-03-16 22:19:42 +0800

    up

--- HappyAutoReply.lua
+++ HappyAutoReply.lua
@@ -3,51 +3,47 @@
 -- [防脚本检测]请在聊天频道回答以下问题的答案:333+444=? ,剩余回答时间为:300秒，超时会被强制下线，连续多次超时会被封号。
 
 -- local testMsg1 = "|cff3DAEFF[防脚本检测]请在聊天频道回答以下问题的答案:|cffFFD800"
-local testMsg1 = "[防脚本检测]请在聊天频道回答以下问题的答案:"
-local testMsg2 = "=? ,剩余回答时间为:300秒，超时会被强制下线，连续多次超时会被封号。"
+local testMsg1              = "[防脚本检测]请在聊天频道回答以下问题的答案:"
+local testMsg2              = "=? ,剩余回答时间为:300秒，超时会被强制下线，连续多次超时会被封号。"
 
 -------------------------------------------------------
-LOGGER = {}
-TARGET_MSG_1 = "防脚本检测"
-TARGET_MSG_2 = "请在聊天频道回答以下问题的答案"
-TARGET_MSG_3 = "剩余回答时间"
-TARGET_MSG_4 = "请在聊天频道回答以下问题的答案:|cffFFD800"
+LOGGER                      = {}
+TARGET_MSG_1                = "防脚本检测"
+TARGET_MSG_2                = "请在聊天频道回答以下问题的答案"
+TARGET_MSG_3                = "剩余回答时间"
+TARGET_MSG_4                = "请在聊天频道回答以下问题的答案:|cffFFD800"
 
-TARGET_MSG_INVITE_CHECK = "555"
-TARGET_MSG_INVITE_APPLY = "666"
+TARGET_MSG_INVITE_CHECK     = "555"
+TARGET_MSG_INVITE_APPLY     = "666"
 
-AUTO_FOLLOW_PLAYER_NAME = "";
-AUTO_FOLLOW_START_MSG   = "111";
-AUTO_FOLLOW_STOP_MSG    = "222";
+AUTO_FOLLOW_PLAYER_NAME     = "";
+AUTO_FOLLOW_START_MSG       = "111";
+AUTO_FOLLOW_STOP_MSG        = "222";
 
-HAR_ACTIVE_FLAG = true;
-TRACE_FLAG = false;
+HAR_ACTIVE_FLAG             = true;
+TRACE_FLAG                  = false;
 
 HappyAutoReplyDataPerCharDB = {}
 -------------------------------------------------------
 
-SLASH_HAR1 = "/har";
-SLASH_HAR2 = "/happyar";
-SlashCmdList["HAR"] = function(msg)
+SLASH_HAR1                  = "/har";
+SLASH_HAR2                  = "/happyar";
+SlashCmdList["HAR"]         = function(msg)
 	local cmd, arg = string.split(" ", msg, 2);
 	cmd = cmd:lower()
 
 	if cmd == "show" then
 		HAR_MainFrame:Show();
-
 	elseif cmd == "hide" then
 		HAR_MainFrame:Hide();
-
 	elseif cmd == "on" then
 		HAR_ACTIVE_FLAG = true;
 		HappyAutoReplyDataPerCharDB.active = true;
-		print("[HappyAutoReply]Active=" .. tostring(HAR_ACTIVE_FLAG) .. "." );
-
+		print("[HappyAutoReply]Active=" .. tostring(HAR_ACTIVE_FLAG) .. ".");
 	elseif cmd == "off" then
 		HAR_ACTIVE_FLAG = false;
 		HappyAutoReplyDataPerCharDB.active = false;
-		print("[HappyAutoReply]Active=" .. tostring(HAR_ACTIVE_FLAG) .. "." );
-
+		print("[HappyAutoReply]Active=" .. tostring(HAR_ACTIVE_FLAG) .. ".");
 	elseif cmd == "trace" then
 		if TRACE_FLAG then
 			TRACE_FLAG = false;
@@ -56,11 +52,9 @@ SlashCmdList["HAR"] = function(msg)
 			TRACE_FLAG = true;
 			HappyAutoReplyDataPerCharDB.trace = true;
 		end
-		print("[HappyAutoReply]Trace=" .. tostring(TRACE_FLAG) .. "." );
-
+		print("[HappyAutoReply]Trace=" .. tostring(TRACE_FLAG) .. ".");
 	elseif cmd == "dump" then
 		HAR_dumpLog();
-
 	elseif cmd == "" then
 		-- print("|cFFFF0000Raid Invite Organizer|r:");
 		print("/har cmd");
@@ -71,29 +65,19 @@ SlashCmdList["HAR"] = function(msg)
 		print("---->trace - switch trace flag.")
 		print("---->dump  - dump all logs.")
 		print("---->xxx   - switch trace flag.")
-		print("[HappyAutoReply]Active=" .. tostring(HAR_ACTIVE_FLAG) .. "." );
-		print("[HappyAutoReply]Trace =" .. tostring(TRACE_FLAG) .. "." );
-
+		print("[HappyAutoReply]Active=" .. tostring(HAR_ACTIVE_FLAG) .. ".");
+		print("[HappyAutoReply]Trace =" .. tostring(TRACE_FLAG) .. ".");
 	else
 		-- LOGGER.debug("arg=" .. arg);
-		-- SendChatMessage(testMsg1 .. cmd .. testMsg2);
+		SendChatMessage(testMsg1 .. cmd .. testMsg2);
 		-- print("" .. msg .. " is not a valid command for /har");
-		
+
 		for i = 1, STATICPOPUP_NUMDIALOGS do
-			local frame = _G["StaticPopup"..i]
+			local frame = _G["StaticPopup" .. i]
 
 			LOGGER.debug(i);
+
 			HAR_dumpFrame(frame);
-			-- print(i .. " --> " .. frame)
-			-- if frame:IsVisible() and frame.which == "PARTY_INVITE" then
-			-- 	frame.inviteAccepted = 1
-			-- 	StaticPopup_Hide("PARTY_INVITE")
-			-- 	return
-			-- elseif frame:IsVisible() and frame.which == "PARTY_INVITE_XREALM" then
-			-- 	frame.inviteAccepted = 1
-			-- 	StaticPopup_Hide("PARTY_INVITE_XREALM")
-			-- 	return
-			-- end
 		end
 	end
 end
@@ -113,11 +97,10 @@ function HAR_EventHandler(self, event, ...)
 		local msg, sendPlayerName = ...;
 		LOGGER.trace("receive SAY message. msg=" .. msg .. ".");
 		if HAR_checkTargetMessage(msg) then
-			LOGGER.debug("---->check this message from "..sendPlayerName..".");
+			LOGGER.debug("---->check this message from " .. sendPlayerName .. ".");
 			HAR_saveLog("CHAT_MSG_SAY", sendPlayerName, msg);
 			HAR_sloveQuestion(msg);
 		end
-
 	elseif event == "CHAT_MSG_SYSTEM" then
 		local msg = ...;
 		LOGGER.trace("receive SYSTEM message. msg=" .. msg .. ".");
@@ -126,34 +109,21 @@ function HAR_EventHandler(self, event, ...)
 			HAR_sloveQuestion(msg);
 			HAR_saveLog("CHAT_MSG_SYSTEM", "SYSTEM", msg);
 		end
-
 	elseif event == "PARTY_INVITE_REQUEST" then
 		local playerName = ...;
 		LOGGER.trace(playerName .. " invite you into a party.");
 		AUTO_FOLLOW_PLAYER_NAME = playerName;
-		AcceptGroup();
-		-- LOGGER.trace("AcceptGroup");
-		-- StaticPopup_Hide("PARTY_INVITE");
-		-- LOGGER.trace("Close PARTY_INVITE Frame.");
 
-		for i = 1, STATICPOPUP_NUMDIALOGS do
-			local frame = _G["StaticPopup"..i]
-			print(i .. " --> " .. frame)
-			if frame:IsVisible() and frame.which == "PARTY_INVITE" then
-				frame.inviteAccepted = 1
-				StaticPopup_Hide("PARTY_INVITE")
-				return
-			elseif frame:IsVisible() and frame.which == "PARTY_INVITE_XREALM" then
-				frame.inviteAccepted = 1
-				StaticPopup_Hide("PARTY_INVITE_XREALM")
-				return
-			end
-		end
+		-- 接受组队邀请
+		AcceptGroup();
+
+		-- 关闭组队邀请窗口
+		HAR_closePartyInviteFrame();
 
 	elseif event == "CHAT_MSG_PARTY" or event == "CHAT_MSG_PARTY_LEADER" then
 		local msgText, playerName, _, _, playerName2 = ...;
 		LOGGER.trace("receive PARTY message. msg=" .. msgText .. ".");
-		
+
 		-- if HAR_isStartFllowMsg(playerName, msgText) then
 		-- 	LOGGER.trace("  start follow by >>" .. playerName .. "<<.");
 		-- 	HAR_followStart();
@@ -210,14 +180,14 @@ function HAR_sloveQuestion(msg)
 
 	-- start pos = find + 2 (skip fullspace character and :)
 	-- end   pos = find - 1 (skip =)
-	local subString = string.sub(msg, posA2, posB1-1)
-	LOGGER.debug(">"..subString.."<");
+	local subString = string.sub(msg, posA2, posB1 - 1)
+	LOGGER.debug(">" .. subString .. "<");
 
-	local val1,operate,val2 = string.match(subString, "(%d+)([%+%-%*/])(%d+)")
+	local val1, operate, val2 = string.match(subString, "(%d+)([%+%-%*/])(%d+)")
 
-	LOGGER.debug( val1 );
-	LOGGER.debug( operate );
-	LOGGER.debug( val2 );
+	LOGGER.debug(val1);
+	LOGGER.debug(operate);
+	LOGGER.debug(val2);
 
 	local numberVal1 = tonumber(val1)
 	local numberVal2 = tonumber(val2)
@@ -232,12 +202,12 @@ function HAR_sloveQuestion(msg)
 	elseif operate == "/" then
 		numberAnswer = numberVal1 / numberVal2
 	end
-	
-	LOGGER.debug( "=" );
-	LOGGER.debug( numberAnswer );
+
+	LOGGER.debug("=");
+	LOGGER.debug(numberAnswer);
 
 	if HAR_ACTIVE_FLAG then
-		SendChatMessage(tostring(numberAnswer));		
+		SendChatMessage(tostring(numberAnswer));
 	end
 end
 
@@ -257,13 +227,13 @@ end
 
 function HAR_followStart()
 	for i = 1, 5 do
-		local strUnitId = "party"..i;
+		local strUnitId = "party" .. i;
 		LOGGER.debug("  check " .. strUnitId)
 		local strUnitName = UnitName(strUnitId);
 		if strUnitName == nil then
 			break;
 		end
-		LOGGER.debug(strUnitId.."="..strUnitName..".");
+		LOGGER.debug(strUnitId .. "=" .. strUnitName .. ".");
 		if strUnitName == AUTO_FOLLOW_PLAYER_NAME then
 			FollowUnit(strUnitId);
 		end
@@ -271,17 +241,19 @@ function HAR_followStart()
 end
 
 function HAR_followStop()
-	FollowUnit( "player" );
-	FollowUnit( "none" );
+	FollowUnit("player");
+	FollowUnit("none");
 end
 
 function HAR_dumpLog()
-	LOGGER.trace( "dump logs." )
+	LOGGER.trace("dump logs.")
 
 	local logSize = #HappyAutoReplyDataPerCharDB.log;
 	for i = 1, logSize, 1 do
 		local log = HappyAutoReplyDataPerCharDB.log[i];
-		LOGGER.debug( "idx=" .. i .. ", datetime=" .. log.datetime .. ", event=" .. log.event .. ", sender=" .. log.sender .. ", text=" .. log.text );
+		LOGGER.debug("idx=" ..
+		i ..
+		", datetime=" .. log.datetime .. ", event=" .. log.event .. ", sender=" .. log.sender .. ", text=" .. log.text);
 	end
 end
 
@@ -296,7 +268,7 @@ function HAR_saveLog(event, sender, msg)
 	checkMsg.text = msg;
 
 	local logSize = #HappyAutoReplyDataPerCharDB.log;
-	if (logSize>5) then
+	if (logSize > 5) then
 		HappyAutoReplyDataPerCharDB.log[1] = HappyAutoReplyDataPerCharDB.log[2];
 		HappyAutoReplyDataPerCharDB.log[2] = HappyAutoReplyDataPerCharDB.log[3];
 		HappyAutoReplyDataPerCharDB.log[3] = HappyAutoReplyDataPerCharDB.log[4];
@@ -306,7 +278,6 @@ function HAR_saveLog(event, sender, msg)
 		local idx = logSize + 1
 		HappyAutoReplyDataPerCharDB.log[idx] = checkMsg;
 	end
-
 end
 
 function HAR_dumpFrame(valFrame)
@@ -315,11 +286,11 @@ function HAR_dumpFrame(valFrame)
 	if frameName == nil then
 		frameName = "null";
 		-- LOGGER.debug( frameName .. ", visable:" .. valFrame:IsVisible() );
-		LOGGER.debug( frameName .. ", visable:false" );
+		LOGGER.debug(frameName .. ", visable:false");
 	else
-		LOGGER.debug( frameName .. ", visable:" .. valFrame:IsVisible() );
-		LOGGER.debug( "  close it!" );
-		StaticPopup_OnClick(valFrame, 1)
+		LOGGER.debug(frameName .. ", visable:" .. valFrame:IsVisible());
+		LOGGER.debug("  close it!");
+		-- StaticPopup_OnClick(valFrame, 1)
 	end
 	-- if frame:IsVisible() and frame.which == "PARTY_INVITE" then
 end
@@ -335,4 +306,14 @@ function HAR_dumpTableValue(valTable)
 	-- 		LOGGER.debug( "  [" .. type(val) .. "]" .. pos .. ":" .. tostring(val) );
 	-- 	end
 	-- end	
+end
+
+function HAR_closePartyInviteFrame()
+	for i = 1, STATICPOPUP_NUMDIALOGS do
+		local frame = _G["StaticPopup" .. i]
+		if (frame:IsVisible() and frame.which == "PARTY_INVITE") then
+			-- 		frame.inviteAccepted = 1
+			StaticPopup_OnClick(frame, 1);
+		end
+	end
 end
